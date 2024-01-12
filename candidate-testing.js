5656b573b29a0a4ea1874c546002c3fe455d420d
Shedd32 <justinshedd21@gmail.com>
2024-01-11 19:45:11 -0600

    GA 1 part 1 done

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,19 +18,19 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
- let candidateName = input.question("What is your name? ")
+  candidateName = input.question("What is your name?")
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-let candidateAnswer = input.question(question);
+   candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log ("Your answer was ", + candidateAnswer, + " The correct answer is ", + correctAnswer)
+console.log ("Your answer was: "+  candidateAnswer +  " The correct answer is: "+  correctAnswer)
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
